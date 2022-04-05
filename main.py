9e4a0c5d34685b1501d40ec0bb6667b7422d198b
Prashant Kesharwani <47141326+Prashant-Kesharwani@users.noreply.github.com>
2022-04-05 14:02:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,86 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import pandas_datareader as data
-from keras.models import load_model
+# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
+from datetime import date
+import matplotlib.pyplot as plt
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+import pandas_datareader as datareader
 
-start = '2010-01-01'
-end = '2019-12-31'
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title('Stock Trend Prediction')
+st.title('Stock Forecast App')
+
+
+selected_stock = st.text_input('Enter Stock Ticker', 'AAPL')
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+
+# Moving Averages
+
+df = datareader.DataReader(selected_stock, 'yahoo', START, TODAY)
+
+st.subheader('Closing Price vs Time chart with 100MA')
+ma100 = df.Close.rolling(100).mean()
+fig = plt.figure(figsize = (20,6))
+plt.plot(ma100, 'lime')
+plt.plot(df.Close, 'b')
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA & 200MA')
+ma100 = df.Close.rolling(100).mean()
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize = (20,6))
+plt.plot(ma100, 'lime')
+plt.plot(ma200, 'r')
+plt.plot(df.Close, 'b')
+st.pyplot(fig)
